745e3f48575ad8ce476bd08e4dbad897d8f9c31a
gawgaw <gawgaw.developer@gmail.com>
2020-10-27 11:46:29 +0900

    ベースファイルを作成

--- js/hamburger-toggle.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('#burger');
-const nav = document.querySelector('nav');
-
-btn.addEventListener('click', () =>{
-  btn.classList.toggle('active');
-  nav.classList.toggle('show');
-});
